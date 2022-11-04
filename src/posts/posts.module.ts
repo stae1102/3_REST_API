517fa58587a544654bf21b51a3a41152ae4a5b6e
Seongtae <stae1102@gmail.com>
2022-11-04 17:01:49 +0900

    chore: Inject HttpModule to post module
    To use axios request, injected dependency for HttpModule
    
    Fixes: #7

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
