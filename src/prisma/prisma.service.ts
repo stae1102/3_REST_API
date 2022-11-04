157620c2fe9598905d9011b9bf56bcb79fd09b35
Seongtae <stae1102@gmail.com>
2022-11-04 23:14:55 +0900

    feat: Add soft delete middleware
    Prisma don't provide the soft delete method not yet.
    So you should insert a soft delete middleware into prisma client.
    
    Fixes: #18

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -14,6 +14,68 @@ export class PrismaService
 
   async onModuleInit() {
     await this.$connect();
+
+    this.$use(async (params, next) => {
+      if (params.action === 'findUnique' || params.action === 'findFirst') {
+        // Change to findFirst - you cannot filter
+        // by anything except ID / unique with findUnique
+        params.action = 'findFirst';
+        // Add 'deleted_at' filter
+        // ID filter maintained
+        params.args.where['deleted_at'] = null;
+      }
+      if (params.action === 'findMany') {
+        // Find many queries
+        if (params.args.where) {
+          if (params.args.where.deleted_at == null) {
+            // Exclude deleted_at records if they have not been explicitly requested
+            params.args.where['deleted_at'] = null;
+          }
+        } else {
+          params.args['where'] = { deleted_at: null };
+        }
+      }
+      return next(params);
+    });
+
+    this.$use(async (params, next) => {
+      if (params.action == 'update') {
+        // Change to updateMany - you cannot filter
+        // by anything except ID / unique with findUnique
+        params.action = 'updateMany';
+        // Add 'deleted_at' filter
+        // ID filter maintained
+        params.args.where['deleted_at'] = null;
+      }
+      if (params.action == 'updateMany') {
+        if (params.args.where != undefined) {
+          params.args.where['deleted_at'] = null;
+        } else {
+          params.args['where'] = { deleted_at: null };
+        }
+      }
+      return next(params);
+    });
+
+    this.$use(async (params, next) => {
+      // Check incoming query type
+      if (params.action == 'delete') {
+        // Delete queries
+        // Change action to an update
+        params.action = 'update';
+        params.args['data'] = { deleted_at: new Date() };
+      }
+      if (params.action == 'deleteMany') {
+        // Delete many queries
+        params.action = 'updateMany';
+        if (params.args.data != undefined) {
+          params.args.data['deleted_at'] = new Date();
+        } else {
+          params.args['data'] = { deleted_at: new Date() };
+        }
+      }
+      return next(params);
+    });
   }
 
   async onModuleDestroy() {
