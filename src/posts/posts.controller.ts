49c578af0b700cdca02ff4f24114bb741626ed28
Seongtae <stae1102@gmail.com>
2022-11-04 17:18:34 +0900

    feat: Create posting feature on post resource
    Created createPost feature
    - api: GET /posts
    - param createDto { title, content, password }
    - returns true
    
    Resolves: #7

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  createPost(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.createPost(createPostDto);
+  }
 }
